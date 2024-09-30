0bdc4b723bd8fe1fb7bed021892cecdb3b087523
MissDana <myrzabekova.dana9@gmail.com>
2024-09-30 16:35:02 +0300

    first commit

---
+++ admin/src/App.tsx
@@ -0,0 +1,59 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Footer from "./components/footer/Footer.tsx";
+import Menu from "./components/menu/menu.tsx";
+import Navbar from "./components/navbar/Navbar.tsx";
+import Home from "./pages/home/Home.tsx";
+import Login from "./pages/login/Login.tsx";
+import Products from "./pages/products/Products.tsx";
+import Users from "./pages/users/Users.tsx";
+import "./styles/global.scss"
+
+function App() {
+const Layout=()=>{
+  return(
+    <div className="main">
+      <Navbar/>
+      <div className="container">
+        <div className="menuContainer">
+          <Menu/>
+        </div>
+        <div className="contentContainer">
+          <Outlet/>
+        </div>
+      </div>
+      <Footer/>
+    </div>
+  );
+};
+
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element:<Layout/>,
+      children:[
+        {
+          path:"/",
+          element:<Home/>
+        },
+        {
+          path:"/users",
+          element:<Users/>
+        },
+        {
+          path:"/products",
+          element:<Products/>
+        },
+      ],
+    },
+    {
+      path:"/login",
+      element:<Login/>,
+    },
+  ]);
+
+  return <RouterProvider router={router} />; 
+}
+
+
+
+export default App;
---
+++ admin/src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Hired Valley</span>
+      <span>© Hired Valley Dev Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
